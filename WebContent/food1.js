ec35327bcf033bda46d5d1e66fe180d9c466356e
minjeong <cpflqhdlrmsu@gmail.com>
2020-09-26 23:04:56 +0900

    Design

--- WebContent/food1.js
+++ WebContent/food1.js
@@ -1,4 +1,11 @@
 $( document ). ready(function() {
-
+    $("#gobtn").click(function(){
+        $('#float *').remove();
+        $('#gobtn').remove();
+        var $foodimgl = $("<img src='신림먹방특공대/점포1.jpeg'/>");
+        var $foodimgr = $("<img src='신림먹방특공대/점포1.jpeg'/>");
+        $('#float').append($foodimgl);
+        $('#float').append($foodimgr);
+    });
     
 });
