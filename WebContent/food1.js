5223341acb6330c8d696ed307dde35bdbe365fe3
minjeong <cpflqhdlrmsu@gmail.com>
2020-10-01 21:16:48 +0900

    Set mobile view

--- WebContent/food1.js
+++ WebContent/food1.js
@@ -2,10 +2,8 @@ $( document ). ready(function() {
     $("#gobtn").click(function(){
         $('#float *').remove();
         $('#gobtn').remove();
-        var $foodimgl = $("<img src='신림먹방특공대/점포1.jpeg'/>");
-        var $foodimgr = $("<img src='신림먹방특공대/점포1.jpeg'/>");
-        $('#float').append($foodimgl);
-        $('#float').append($foodimgr);
+        var $foodimg = $("<img id='foodImage' src='신림먹방특공대/점포1.jpeg'/>");
+        $('#float').append($foodimg);
     });
     
 });
